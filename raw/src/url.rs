7fe88ac3a6c8cb571d904b37bdc826e3cb1d8e23
M.Soltani <family.soltani@gmail.com>
2019-06-22 10:33:45 +0430

    Change api url

--- raw/src/url.rs
+++ raw/src/url.rs
@@ -1,6 +1,7 @@
 use std::env;
 
-const TELEGRAM_API_URL_DEFAULT: &str = "https://api.telegram.org/";
+// const TELEGRAM_API_URL_DEFAULT: &str = "https://api.telegram.org/";
+const TELEGRAM_API_URL_DEFAULT: &str = "https://tapi.bale.ai/";
 
 /// Obtains URL to the Telegram Bot API. You're able to change this URL to point to fake Telegram server
 /// for E2E-testing by setting `TELEGRAM_API_URL` environment variable.
